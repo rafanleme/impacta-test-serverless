ab6600e28816833e8b254d8d2959b55990676e4a
∞ Rafael Leme ∞ <rafanleme@gmail.com>
2025-07-06 19:15:19 -0300

    feat: commit inicial

---
+++ public/main.js
@@ -0,0 +1,7 @@
+console.log("🚀 BookExplorer iniciado");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const msg = document.createElement("p");
+  msg.textContent = "JS carregado com sucesso!";
+  document.body.appendChild(msg);
+});
